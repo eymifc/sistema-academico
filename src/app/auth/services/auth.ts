a3cd58cd8f2a1e0d03a635511b4ab76dcaf5ef37
udemygrupo8-create <udemygrupo8@gmail.com>
2025-10-05 20:54:36 -0400

    guardando datos en localstorage

--- src/app/auth/services/auth.ts
+++ src/app/auth/services/auth.ts
@@ -1,4 +1,3 @@
-
 import { Observable, tap } from 'rxjs';
 import { jwtDecode } from 'jwt-decode';
 import { UserProfile, UserProfileService } from '../../core/services/user-profile';
@@ -11,28 +10,47 @@ import {HttpClient} from '@angular/common/http';
 
 @Injectable({ providedIn: 'root' })
 export class Auth {
-    private apiUrl = 'http://localhost:8080/api/v1/auth';
+  private apiUrl = 'http://localhost:8080/api/v1/auth';
   constructor(private http: HttpClient, private userProfileService: UserProfileService, private router: Router) { }
 
   login(credentials: {login: string, password: string}): Observable<any> {
     return this.http.post<any>(`${this.apiUrl}/login`, credentials).pipe(
       tap(response => {
 
-        const decodedToken = jwtDecode<UserProfile>(response.token);
+
+        const decodedToken: any = jwtDecode(response.token);
         this.userProfileService.setCurrentUser(decodedToken);
 
         localStorage.setItem('jwt_token', response.token);
+
+
+        localStorage.setItem('username', decodedToken.usuario);
+
+
+        localStorage.setItem('fullName', decodedToken.nombreCompleto);
+
+
+        localStorage.setItem('loginDate', decodedToken.fecha_creacion);
+
+
+        localStorage.setItem('roles', JSON.stringify(decodedToken.roles));
+
+
       })
     );
   }
 
-logout(): void {
+  logout(): void {
+
     localStorage.removeItem('jwt_token');
+    localStorage.removeItem('username');
+    localStorage.removeItem('fullName');
+    localStorage.removeItem('loginDate');
+    localStorage.removeItem('roles');
+
     this.userProfileService.setCurrentUser(null);
-
     this.router.navigate(['/']);
-
-    console.log('Usuario desconectado.');
+    console.log('Usuario desconectado y datos de sesión eliminados.');
   }
 
 }
